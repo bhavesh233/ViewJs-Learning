52a1795cd07b30f32d96f6b68a86c5dbd92bf439
Bhavesh233 <bhaveshk@prominentpixel.com>
2022-01-11 19:13:28 +0530

    create forms

--- component-project/src/App.vue
+++ component-project/src/App.vue
@@ -1,52 +1,34 @@
 <template>
-    <ul>
-      <learning-resource
-        v-for="res in storeResources"
-        :key="res.id"
-        :title="res.title"
-        :description="res.description"
-        :link="res.link"
-      >
-      </learning-resource>
-    </ul>
-  
+  <div>
+    <the-header title="RememberMe"></the-header>
+    <the-resources></the-resources>
+  </div>
 </template>
 
 <script>
-import LearningResource from "./components/learning-resource/LearningResource.vue";
+import TheHeader from "./components/layouts/TheHeader.vue";
+import TheResources from "./components/learning-resources/TheResources.vue";
 
 export default {
   components: {
-    LearningResource,
-  },
-  data() {
-    return {
-      storeResources: [
-        {
-          id: "offcial-guide",
-          title: "Official Guide",
-          description: "the official vue.js documentations",
-          link: "https://vue.js.org",
-        },
-        {
-          id: "google",
-          title: "Google",
-          description: "Learn to google",
-          link: "https://google.org",
-        },
-      ],
-    };
+    TheHeader,
+    TheResources,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
